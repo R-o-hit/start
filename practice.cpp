88330256429c40465b66af12fe0f749f47ea7999
Rohit kumar <rohitkumarrkrd@gmail.com>
2022-09-20 16:54:30 +0530

    add file c++

---
+++ practice.cpp
@@ -0,0 +1,23 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main()
+{ int k=0;
+int num;
+cin>>num;
+	while(num!=0)
+	{ k++;
+	if(num%2==0)
+	{
+		num=num/2;
+		
+	}
+	else
+	num=num-1;
+	
+		
+	}
+	cout<<k;
+	return 0;
+	
+	
+}
